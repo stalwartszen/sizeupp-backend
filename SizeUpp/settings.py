d94806d5e03be138a40f8fa6157589bbfef583b6
Hrithik Hadawale <hrithikhadawale75@gmail.com>
2023-12-11 13:34:07 +0530

    fwefw

--- SizeUpp/settings.py
+++ SizeUpp/settings.py
@@ -69,6 +69,8 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
