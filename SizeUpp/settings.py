315d12b6cc51e6ae4bbfc79eb1da606f076099f0
Hrithik Hadawale <hrithikhadawale75@gmail.com>
2023-12-11 14:11:56 +0530

    FWfwfwf

--- SizeUpp/settings.py
+++ SizeUpp/settings.py
@@ -139,6 +139,9 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "authentication.User"
