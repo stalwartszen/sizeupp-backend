8db7ac67d0885a24ccca6f16642be43994462f6c
Hrithik Hadawale <hrithikhadawale75@gmail.com>
2023-12-12 19:36:48 +0530

    ffe

--- SizeUpp/settings.py
+++ SizeUpp/settings.py
@@ -138,7 +138,7 @@ MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 
 
